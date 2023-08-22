e760e765f661cc7b24172ac3fdc4ee2d7cecbe6f
sabitha-K <141926806+sabitha-K@users.noreply.github.com>
2023-08-22 23:31:33 +0530

    Update Hangman_project.py

--- Hangman_project.py
+++ Hangman_project.py
@@ -1,8 +1,9 @@
 import random
 lives=6
-words=["apple", "orange", "potato"]
+words=["apple", "orange", "grapes"]
+print("Guess a fruit name , You have six lives!")
 chosen_word=random.choice(words)
-print(chosen_word)
+# print(chosen_word)
 display=[]
 for i in range(len(chosen_word)):
     display+="_"
@@ -13,18 +14,17 @@ while not game_over:
     for position in range(len(chosen_word)):
         letter=chosen_word[position]
         if letter==guessed_letter:
-            display[position]=guessed_letter
+          display[position]=guessed_letter
     print(display)
     if guessed_letter not in chosen_word:
         lives-=1
+        print("You Have",lives, "lives")
         if lives==0:
             game_over=True
             print("You Lose")
     if "_" not in display:
         game_over=True
         print("You Won")
-
-
 
 
 
